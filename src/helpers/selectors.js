e57624e732b8cf3c2ca761d0b6488428cf04bc42
Darren Beattie <darren@fettle.ca>
2020-01-21 18:20:55 -0500

    add getAppointmentsForDay helper function (selector) based on Jest Unit tests provided

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+export function getAppointmentsForDay(state, day) {
+  const returnArr = [];
+  const filteredAppointments = state.days.filter(d => d.name === day);
+
+  if (filteredAppointments.length === 0) {
+    return returnArr;
+  } else {
+    for (let id of filteredAppointments[0].appointments) {
+      returnArr.push(state.appointments[id]);
+    }
+  }
+  return returnArr;
+};
