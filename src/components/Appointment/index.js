3790031dd6b92d05c593bafe4eb0481f4a975755
Darren Beattie <darren@fettle.ca>
2020-01-21 13:11:53 -0500

    add Empty Appointment component and story

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import "./styles.scss";
 
-// import Header from "Appointment/Header.js";
-// import Show from "Appoinment/Show.js";
-// import Empty from "Appointment/Empty.js";
+import Header from "components/Appointment/Header.js";
+import Show from "components/Appointment/Show.js";
+import Empty from "components/Appointment/Empty.js";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
+  );
 }
