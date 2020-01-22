8f708bb6f67cf7899eeba59a5684e120c6c83a2d
Darren Beattie <darren@fettle.ca>
2020-01-22 18:37:01 -0500

    create getInterviewersForDay function, create JEST tests, and add test data for them

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,3 @@
-
 function getAppointmentsForDay(state, day) {
   const returnArr = [];
   const filteredAppointments = state.days.filter(d => d.name === day);
@@ -25,4 +24,40 @@ function getInterview(state, interview) {
   }
 }
 
-module.exports = { getAppointmentsForDay, getInterview };
+function getInterviewersForDay(state, day) {
+  const returnArr = [];
+  const filteredInterviewers = state.days.filter(d => d.name === day);
+
+  if (filteredInterviewers.length === 0) {
+    return returnArr;
+  } else {
+    for (let id of filteredInterviewers[0].interviewers) {
+      returnArr.push(state.interviewers[id]);
+    }
+  }
+  return returnArr;
+};
+
+// GET THE INTERVIEWER ID's FOR EACH DAY BASED ON THE APPOINTMENTS KEEP FOR REFERENCE???****
+// function getInterviewersForDay(state, day) {
+//   const returnArr = [];
+//   const appointmentNumbers = state.days.filter(d => d.name === day);
+
+//   console.log('FILTERED APPOINTMENT ##:-->', appointmentNumbers[0].appointments);
+
+//   if (appointmentNumbers.length === 0) {
+//     return returnArr;
+//   } else {
+//     for (let interviewerId of appointmentNumbers[0].appointments) {
+//       if (state.appointments[interviewerId].interview !== null) {  
+//       console.log('Interview Number LOOP:-->', state.appointments[id].interview.interviewer);
+//       returnArr.push(state.appointments[id].interview.interviewer);
+//       }
+//     }
+//   }
+//   console.log('RETURN ARRAY:-->', returnArr);
+//   return returnArr;
+// };
+
+
+module.exports = { getAppointmentsForDay, getInterview, getInterviewersForDay };
