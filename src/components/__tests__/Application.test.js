4fd639943b1fb0779a5d294f8822b43eaa5f14eb
Darren Beattie <darren@fettle.ca>
2020-01-27 12:49:08 -0500

    add Appointment component jest test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,6 +6,9 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
-});
+describe("Application", () => {
+  it("renders without crashing", () => {
+    render(<Application />);
+  });
+})
+
