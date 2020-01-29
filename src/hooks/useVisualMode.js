399a9ddd608f8fc929d314231f633dec57c1e26f
Darren Beattie <darren@fettle.ca>
2020-01-29 13:24:00 -0500

    final lint/check before submission

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export default function useVisualMode(initialMode) {
-  // const [mode, setMode] = useState(initialMode);
   const [history, setHistory] = useState([initialMode]);
 
   function transition(mode, replace = false)  {
